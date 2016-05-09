c06d6fda1e5f75553d056054603dd531473b6845
iansun <lauer3912@gmail.com>
2016-05-09 14:21:46 +0800

    update

---
+++ js/config.js
@@ -0,0 +1,15 @@
+(function () {
+    window.RTYConfig = {
+        appName:"CSVKiller",
+        appID:"com.romanysoft.app.macos.CSVKiller",
+        documentTitle:"CSVKiller By Romanysoft",
+        supportPlatforms:["Mac"],
+        googleUA:"UA-76676167-13",
+        gitHome:"//github.com/Romanysoft/CSVKiller",
+        reportIssueUrl:"//github.com/Romanysoft/CSVKiller/issues",
+        changeLogUrl:"//github.com/Romanysoft/CSVKiller/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/CSVKiller/wiki",
+        romanysoftHome:"//www.romanysoft.com",
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    }
+})();
